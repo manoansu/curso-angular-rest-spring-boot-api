6d6e1c1f8b0fa824e684e87fbf386ab9054b9ffb
Ansumane Mané <manoansu@gmail.com>
2022-02-08 18:42:54 +0000

    Start project

---
+++ curso-angular-rest-spring-boot-api/src/main/java/pt/amane/cursoangularrestspringbootapi/CursoAngularRestSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package pt.amane.cursoangularrestspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoAngularRestSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoAngularRestSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ curso-angular-rest-spring-boot-api/src/test/java/pt/amane/cursoangularrestspringbootapi/CursoAngularRestSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.amane.cursoangularrestspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoAngularRestSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
