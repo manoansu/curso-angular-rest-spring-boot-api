4c1b7871324ea06d45d6a24ff44933636fed8d35
Ansumane Mané <manoansu@gmail.com>
2022-02-17 09:29:32 +0000

    Lancamento pesquisa e cadastro, Pessoa pesquisa cadastro sao implementado

--- algamoney-frontend/src/app/app.module.ts
+++ algamoney-frontend/src/app/app.module.ts
@@ -4,25 +4,45 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { InputTextareaModule } from "primeng/inputtextarea";
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentosCadastroComponent } from './lancamentos-cadastro/lancamentos-cadastro.component';
+import { CalendarModule } from 'primeng/calendar';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { PessoasCadastradoComponent } from './pessoas-cadastrado/pessoas-cadastrado.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LancamentosPesquisaComponent,
-    NavbarComponent
+    NavbarComponent,
+    PessoasPesquisaComponent,
+    LancamentosCadastroComponent,
+    PessoasCadastradoComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     InputTextModule,
+    InputNumberModule,    
     ButtonModule,
+    DropdownModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    TableModule,
+    SelectButtonModule
+    
 
 
   ],
